92771857a9e113475ce981a73cfb239daeb211b6
James Reyes <reyes.james04m@gmail.com>
2026-08-31 14:34:15 -0500

    Crea un programa principal

---
+++ main.py
@@ -0,0 +1,7 @@
+print("===================================")
+print("PROYECTO DE DESARROLLO DE  SOFTWARE")
+print("===================================")
+
+print("Bienvenido al proyecto")
+print("Estudiantes: James Reyes y Felipe Paez ")
+print("Curso: 10A")
