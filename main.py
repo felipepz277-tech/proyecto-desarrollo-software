98501c3646fd7a08e71781196d203c3f6cb8d621
James Reyes <reyes.james04m@gmail.com>
2026-08-31 15:08:56 -0500

    agrega funciones matematicas

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ print("===================================")
 print("Bienvenido al proyecto")
 print("Estudiantes: James Reyes y Felipe Paez ")
 print("Curso: 10A")
+
+from operaciones import sumar, restar, multiplicar, dividir
+
+print("==============================")
+print("     CALCULADORA EN PYTHON")
+print("==============================")
+
+num1 = float(input("Digite el primer número: "))
+num2 = float(input("Digite el segundo número: "))
+
+print("\nResultados:")
+
+print("Suma:", sumar(num1, num2))
+print("Resta:", restar(num1, num2))
+print("Multiplicación:", multiplicar(num1, num2))
+print("División:", dividir(num1, num2))
